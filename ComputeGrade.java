59222599fa68df1f73495dffa47d981af6e008a4
micoverde1 <micoverde1@gmail.com>
2016-09-16 12:24:10 -0400

    First commit

---
+++ ComputeGrade.java
@@ -0,0 +1,53 @@
+import java.util.Scanner;
+
+public class ComputeGrade{
+
+  public static void main (String[] args){
+ 
+    Scanner input = new Scanner(System.in);
+    
+    System.out.println("Student's name: ");
+    
+    String name = input.next();
+    
+    System.out.println("Enter grade for test 1: ");
+    
+    double grade1 = input.nextDouble();
+  
+    System.out.println("Enter grade for test 2: ");
+    
+    double grade2 = input.nextDouble();
+    
+    System.out.println("Enter grade for test 3: ");
+    
+    double grade3 = input.nextDouble();
+   
+    // finding the average
+    
+    double average = (grade1 + grade2 + grade3) / 3;
+    
+    String finalGrade = name + "'s final grade is a " + getLetterGrade();
+    
+    System.out.println(finalGrade);
+    
+    public static char getLetterGrade(double grade){
+      
+         if(average >= 90){
+      return 'A';
+    }
+    else if(average < 90 && average >= 80){
+      return 'B';
+    }
+    else if(average < 80 && average >= 70){
+      return 'C';
+    }
+    else if(average < 70 && average >= 65){
+      return 'D';
+    }
+      return 'F';
+    }
+      
+    }
+    
+  }
+}
