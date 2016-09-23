1aa4e5f7c802f5b922be6dac0603fdf6082facb1
micoverde1 <micoverde1@gmail.com>
2016-09-23 11:54:37 -0400

    loops

---
+++ TestLoops.java
@@ -0,0 +1,11 @@
+public class TestLoops{
+
+  public static void main(String[] args){ 
+    
+    int x = 2;
+      
+      for(int index = 0; index < 10; index++){
+        System.out.println(x*index);
+      }
+  }
+}
